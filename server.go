ca7cde8b3594b24ac66e6cff36141fdd37feea1e
Raphael J. Lopes <raphaeljlps@gmail.com>
2015-07-02 20:38:24 -0300

    delete endpoint working
    delete endpoint working, json error messages fixed

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
@@ -46,10 +47,11 @@ func main() {
 	router := web.New(Context{})
 	router.Middleware((*Context).AuthorizationMiddleware)
 	router.Middleware(web.LoggerMiddleware)
-	router.Post("/todo/new", (*Context).SaveTask)
-	router.Get("/todo/show/:id", (*Context).GetTask)
-	router.Put("/todo/edit/:id", (*Context).UpdateTask)
-	router.Delete("/todo/delete/:id", (*Context).DeleteTask)
+	router.Get("/todo/:id", (*Context).GetTask)
+	router.Post("/todo", (*Context).SaveTask)
+	router.Put("/todo/:id", (*Context).UpdateTask)
+	router.Delete("/todo/:id", (*Context).DeleteTask)
+	router.NotFound((*Context).NotFound)
 	http.ListenAndServe("localhost:3000", router)
 }
 
@@ -58,29 +60,29 @@ type Context struct {
 }
 
 func (c *Context) GetTask(rw web.ResponseWriter, req *web.Request) {
-	// // gorm, _ := db.InitDB("zeus:omgworked@tcp(localhost:3306)/taskdb")
-	// // m := dbdb.NewTaskManager(gorm)
-	// // task := db.Task{Id: 0, Title: "Hello", Description: "desc", Priority: 10, CreatedAt: 1231231, UpdatedAt: 12312411, CompletedAt: 1231231, IsDeleted: false, IsCompleted: false}
-	//
-	// // err := m.Create(&task)
-	// // log.Println(err)
-
-	idParam := req.PathParams["id"]
-	id, err := strconv.Atoi(idParam)
+	id, err := idFromParam(req)
 	if err != nil {
 		rw.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(rw, "Missing id param")
+		fmt.Fprint(rw, jsonError("missing id param"))
 		return
 	}
 
 	task, err := man.Get(id)
 	if err != nil {
 		rw.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(rw, "Task not found")
+		fmt.Fprint(rw, jsonError("task not found"))
+		return
 	}
 
-	fmt.Fprintf(rw, "Task %d - %s\n", task.Id, task.Title)
+	jsonTask, err := json.Marshal(task)
+	if err != nil {
+		log.Println(err)
+		rw.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(rw, jsonError("error building response"))
+		return
+	}
 
+	fmt.Fprint(rw, string(jsonTask))
 }
 
 func (c *Context) SaveTask(rw web.ResponseWriter, req *web.Request) {
@@ -92,11 +94,32 @@ func (c *Context) UpdateTask(rw web.ResponseWriter, req *web.Request) {
 }
 
 func (c *Context) DeleteTask(rw web.ResponseWriter, req *web.Request) {
-	fmt.Fprint(rw, "Hello")
+	id, err := idFromParam(req)
+	if err != nil {
+		rw.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(rw, jsonError("missing id param"))
+		return
+	}
+
+	err = man.Delete(id)
+	if err != nil {
+		rw.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(rw, jsonError("resource not found"))
+		return
+	}
+
+	rw.WriteHeader(http.StatusNoContent)
+}
+
+func (c *Context) NotFound(rw web.ResponseWriter, req *web.Request) {
+	rw.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(rw, jsonError("resource not found"))
+
 }
 
 //AuthorizationMiddleware gets the Authorization header and verify if is valid
-func (c *Context) AuthorizationMiddleware(rw web.ResponseWriter, r *web.Request, next web.NextMiddlewareFunc) {
+func (c *Context) AuthorizationMiddleware(rw web.ResponseWriter, r *web.Request,
+	next web.NextMiddlewareFunc) {
 	authArray := r.Header["Authorization"]
 	if len(authArray) > 0 {
 		authorization := strings.TrimSpace(authArray[0])
@@ -109,14 +132,25 @@ func (c *Context) AuthorizationMiddleware(rw web.ResponseWriter, r *web.Request,
 				next(rw, r)
 			} else {
 				rw.WriteHeader(http.StatusForbidden)
-				rw.Write([]byte(`{\"error\":\"Invalid token\"}`))
+				fmt.Fprint(rw, jsonError("invalid token"))
 			}
 		} else {
 			rw.WriteHeader(http.StatusForbidden)
-			rw.Write([]byte(`{\"error\":\"Missing token\"}`))
+			fmt.Fprint(rw, jsonError("missing token"))
 		}
 	} else {
 		rw.WriteHeader(http.StatusForbidden)
-		rw.Write([]byte(`{\"error\":\"Missing auth\"}`))
+		fmt.Fprint(rw, jsonError("missing authorization header"))
 	}
+}
+
+//Utility
+func idFromParam(req *web.Request) (id int, err error) {
+	idParam := req.PathParams["id"]
+	id, err = strconv.Atoi(idParam)
+	return
+}
+
+func jsonError(message string) string {
+	return fmt.Sprintf("{\"error\":\"%s\"}", message)
 }
